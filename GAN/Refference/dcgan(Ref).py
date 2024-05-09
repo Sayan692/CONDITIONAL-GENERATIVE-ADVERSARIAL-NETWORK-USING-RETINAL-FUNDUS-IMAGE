d8823cbe2de1e932fc6ecd9bbcbe1a155d8ea730
Sayan692 <76908622+Sayan692@users.noreply.github.com>
2024-05-09 09:58:52 +0530

    Add files via upload

---
+++ GAN/Refference/dcgan(Ref).py
@@ -0,0 +1,248 @@
+import os
+import sys
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+import torchvision.datasets as dset
+import torchvision.utils as vutils
+import matplotlib.pyplot as plt
+from torch.autograd import Variable
+import cv2
+
+CUDA = True
+DATA_PATH = 'C:/Users/pc/Desktop/(T,S,A,L,R)/Test'
+BATCH_SIZE = 128
+IMAGE_CHANNEL = 3
+Z_DIM = 100
+G_HIDDEN = 64
+X_DIM = 128
+D_HIDDEN = 64
+EPOCH_NUM = 60
+REAL_LABEL = 1
+FAKE_LABEL = 0
+lr = 2e-4
+seed = 1
+
+CUDA = CUDA and torch.cuda.is_available()
+print("PyTorch version: {}".format(torch.__version__))
+if CUDA:
+    print("CUDA version: {}\n".format(torch.version.cuda))
+
+if CUDA:
+    torch.cuda.manual_seed(seed)
+device = torch.device("cuda" if CUDA else "cpu")
+cudnn.benchmark = True
+
+# Data preprocessing
+dataset = dset.ImageFolder(root=DATA_PATH,
+                     transform=transforms.Compose([
+                     transforms.Resize(X_DIM),
+                     transforms.ToTensor(),
+                     transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+                     ]))
+
+# Dataloader
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE,
+                                         shuffle=True, num_workers=6)
+
+def normal_init(m, mean, std):
+    if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
+        m.weight.data.normal_(mean, std)
+        m.bias.data.zero_()
+
+
+class generator(nn.Module):
+    # initializers
+    def __init__(self, d=G_HIDDEN):
+        super(generator, self).__init__()
+        self.deconv1 = nn.ConvTranspose2d(Z_DIM, d*16, 4, 1, 0)
+        self.deconv1_bn = nn.BatchNorm2d(d*16)
+        self.deconv2 = nn.ConvTranspose2d(d*16, d*8, 4, 2, 1)
+        self.deconv2_bn = nn.BatchNorm2d(d*8)
+        self.deconv3 = nn.ConvTranspose2d(d*8, d*4, 4, 2, 1)
+        self.deconv3_bn = nn.BatchNorm2d(d*4)
+        self.deconv4 = nn.ConvTranspose2d(d*4, d*2, 4, 2, 1)
+        self.deconv4_bn = nn.BatchNorm2d(d*2)
+        self.deconv5 = nn.ConvTranspose2d(d*2, d, 4, 2, 1)
+        self.deconv5_bn = nn.BatchNorm2d(d)
+        self.deconv6 = nn.ConvTranspose2d(d, 3, 4, 2, 1)
+
+    # weight_init
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+    # forward method
+    def forward(self, input):
+        # x = F.relu(self.deconv1(input))
+        x = F.relu(self.deconv1_bn(self.deconv1(input)))
+        x = F.relu(self.deconv2_bn(self.deconv2(x)))
+        x = F.relu(self.deconv3_bn(self.deconv3(x)))
+        x = F.relu(self.deconv4_bn(self.deconv4(x)))
+        x = F.relu(self.deconv5_bn(self.deconv5(x)))
+        x = F.tanh(self.deconv6(x))
+
+        return x
+
+class discriminator(nn.Module):
+    # initializers
+    def __init__(self, d=D_HIDDEN):
+        super(discriminator, self).__init__()
+        self.conv1 = nn.Conv2d(3, d, 4, 2, 1)
+        self.conv2 = nn.Conv2d(d, d*2, 4, 2, 1)
+        self.conv2_bn = nn.BatchNorm2d(d*2)
+        self.conv3 = nn.Conv2d(d*2, d*4, 4, 2, 1)
+        self.conv3_bn = nn.BatchNorm2d(d*4)
+        self.conv4 = nn.Conv2d(d*4, d*8, 4, 2, 1)
+        self.conv4_bn = nn.BatchNorm2d(d*8)
+        self.conv5 = nn.Conv2d(d*8, d*16, 4, 2, 1)
+        self.conv5_bn = nn.BatchNorm2d(d*16)
+        self.conv6 = nn.Conv2d(d*16, 1, 4, 1, 0)
+
+    # weight_init
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+    # forward method
+    def forward(self, input):
+        x = F.leaky_relu(self.conv1(input), 0.2)
+        x = F.leaky_relu(self.conv2_bn(self.conv2(x)), 0.2)
+        x = F.leaky_relu(self.conv3_bn(self.conv3(x)), 0.2)
+        x = F.leaky_relu(self.conv4_bn(self.conv4(x)), 0.2)
+        x = F.leaky_relu(self.conv5_bn(self.conv5(x)), 0.2)
+        x = F.sigmoid(self.conv6(x))
+
+        return x
+    
+# Create the generator
+netG = generator()
+# netG = nn.DataParallel(netG)
+netG.to(device)
+print(netG)
+
+# Create the discriminator
+netD = discriminator()
+# netD = nn.DataParallel(netD)
+netD.to(device)
+print(netD)
+
+# Initialize BCELoss function
+criterion = nn.BCELoss()
+
+# Create batch of latent vectors that I will use to visualize the progression of the generator
+viz_noise = torch.randn(BATCH_SIZE, Z_DIM, 1, 1, device=device)
+
+# Setup Adam optimizers for both G and D
+optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(0.5, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(0.5, 0.999))
+
+# Training Loop
+
+# Lists to keep track of progress
+img_list = []
+G_losses = []
+D_losses = []
+iters = 0
+
+if __name__=='__main__':
+    print("Starting Training Loop...")
+    #netG.load_state_dict(torch.load('C://Users//pc//CelebA//generator.weight'))
+    #netD.load_state_dict(torch.load('C://Users//pc//CelebA//discriminator.weight'))
+    
+    for epoch in range(EPOCH_NUM):
+        for i, data in enumerate(dataloader, 0):
+    
+            # (1) Update the discriminator with real data
+            netD.zero_grad()
+            # Format batch
+            real_cpu = data[0].to(device)
+            b_size = real_cpu.size(0)
+    
+            label = torch.full((b_size,), REAL_LABEL, dtype=torch.float, device=device)
+            # Forward pass real batch through D
+            output = netD(real_cpu).view(-1)
+    
+            # Calculate loss on all-real batch
+            errD_real = criterion(output, label)
+            # Calculate gradients for D in backward pass
+            errD_real.backward()
+            D_x = output.mean().item()
+    
+            # (2) Update the discriminator with fake data
+            # Generate batch of latent vectors
+            noise = torch.randn(b_size, Z_DIM, 1, 1, device=device)
+            # Generate fake image batch with G
+            fake = netG(noise)
+            label.fill_(FAKE_LABEL)
+            # Classify all fake batch with D
+            output = netD(fake.detach()).view(-1)
+            # Calculate D's loss on the all-fake batch
+            errD_fake = criterion(output, label)
+            # Calculate the gradients for this batch, accumulated (summed) with previous gradients
+            errD_fake.backward()
+            D_G_z1 = output.mean().item()
+            # Compute error of D as sum over the fake and the real batches
+            errD = errD_real + errD_fake
+            # Update D
+            optimizerD.step()
+    
+            # (3) Update the generator with fake data
+            netG.zero_grad()
+            label.fill_(REAL_LABEL)  # fake labels are real for generator cost
+            # Since we just updated D, perform another forward pass of all-fake batch through D
+            output = netD(fake).view(-1)
+            # Calculate G's loss based on this output
+            errG = criterion(output, label)
+            # Calculate gradients for G
+            errG.backward()
+            D_G_z2 = output.mean().item()
+            # Update G
+            optimizerG.step()
+    
+            # Output training stats
+            if i % 50 == 0:
+                print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                      % (epoch, EPOCH_NUM, i, len(dataloader),
+                         errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+    
+            # Save Losses for plotting later
+            G_losses.append(errG.item())
+            D_losses.append(errD.item())
+    
+            # Check how the generator is doing by saving G's output on fixed_noise
+            if (iters % 500 == 0) or ((epoch == EPOCH_NUM-1) and (i == len(dataloader)-1)):
+                with torch.no_grad():
+                    fake = netG(viz_noise).detach().cpu()
+                img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+    
+            iters += 1
+    print("........Running.......")
+    torch.save(netG.state_dict(), 'C://Users//pc//Desktop//(T,S,A,L,R)//generator.weight')
+    torch.save(netD.state_dict(), 'C://Users//pc//Desktop//(T,S,A,L,R)//discriminator.weight')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ GAN/Refference/dcgan_test(Ref).py
@@ -0,0 +1,156 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+import torchvision.datasets as dset
+import torchvision.utils as vutils
+import matplotlib.pyplot as plt
+from torch.autograd import Variable
+import cv2
+
+CUDA = True
+DATA_PATH = 'C:/Users/pc/Desktop/(T,S,A,L,R)/Test'
+BATCH_SIZE = 128
+IMAGE_CHANNEL = 3
+Z_DIM = 100
+G_HIDDEN = 64
+X_DIM = 128
+D_HIDDEN = 64
+EPOCH_NUM = 1
+REAL_LABEL = 1
+FAKE_LABEL = 0
+lr = 2e-4
+seed = 1
+
+def normal_init(m, mean, std):
+    if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
+        m.weight.data.normal_(mean, std)
+        m.bias.data.zero_()
+
+
+class generator(nn.Module):
+    # initializers
+    def __init__(self, d=G_HIDDEN):
+        super(generator, self).__init__()
+        self.deconv1 = nn.ConvTranspose2d(Z_DIM, d*16, 4, 1, 0)
+        self.deconv1_bn = nn.BatchNorm2d(d*16)
+        self.deconv2 = nn.ConvTranspose2d(d*16, d*8, 4, 2, 1)
+        self.deconv2_bn = nn.BatchNorm2d(d*8)
+        self.deconv3 = nn.ConvTranspose2d(d*8, d*4, 4, 2, 1)
+        self.deconv3_bn = nn.BatchNorm2d(d*4)
+        self.deconv4 = nn.ConvTranspose2d(d*4, d*2, 4, 2, 1)
+        self.deconv4_bn = nn.BatchNorm2d(d*2)
+        self.deconv5 = nn.ConvTranspose2d(d*2, d, 4, 2, 1)
+        self.deconv5_bn = nn.BatchNorm2d(d)
+        self.deconv6 = nn.ConvTranspose2d(d, 3, 4, 2, 1)
+
+    # weight_init
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+    # forward method
+    def forward(self, input):
+        # x = F.relu(self.deconv1(input))
+        x = F.relu(self.deconv1_bn(self.deconv1(input)))
+        x = F.relu(self.deconv2_bn(self.deconv2(x)))
+        x = F.relu(self.deconv3_bn(self.deconv3(x)))
+        x = F.relu(self.deconv4_bn(self.deconv4(x)))
+        x = F.relu(self.deconv5_bn(self.deconv5(x)))
+        x = F.tanh(self.deconv6(x))
+
+        return x
+
+class discriminator(nn.Module):
+    # initializers
+    def __init__(self, d=D_HIDDEN):
+        super(discriminator, self).__init__()
+        self.conv1 = nn.Conv2d(3, d, 4, 2, 1)
+        self.conv2 = nn.Conv2d(d, d*2, 4, 2, 1)
+        self.conv2_bn = nn.BatchNorm2d(d*2)
+        self.conv3 = nn.Conv2d(d*2, d*4, 4, 2, 1)
+        self.conv3_bn = nn.BatchNorm2d(d*4)
+        self.conv4 = nn.Conv2d(d*4, d*8, 4, 2, 1)
+        self.conv4_bn = nn.BatchNorm2d(d*8)
+        self.conv5 = nn.Conv2d(d*8, d*16, 4, 2, 1)
+        self.conv5_bn = nn.BatchNorm2d(d*16)
+        self.conv6 = nn.Conv2d(d*16, 1, 4, 1, 0)
+
+    # weight_init
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+    # forward method
+    def forward(self, input):
+        x = F.leaky_relu(self.conv1(input), 0.2)
+        x = F.leaky_relu(self.conv2_bn(self.conv2(x)), 0.2)
+        x = F.leaky_relu(self.conv3_bn(self.conv3(x)), 0.2)
+        x = F.leaky_relu(self.conv4_bn(self.conv4(x)), 0.2)
+        x = F.leaky_relu(self.conv5_bn(self.conv5(x)), 0.2)
+        x = F.sigmoid(self.conv6(x))
+
+        return x
+
+if __name__=='__main__':
+    device = torch.device("cuda" if CUDA else "cpu")
+    
+    netG = generator()
+    netD = discriminator()
+    
+    netG.load_state_dict(torch.load('C://Users//pc//Desktop//(T,S,A,L,R)//generator.weight'))
+    netG.to(device)
+    
+    noise = torch.randn(1, Z_DIM, 1, 1, device=device)
+    with torch.no_grad():
+        fake = netG(noise).detach().cpu().numpy()
+        
+    fake = fake[0]
+    fake = np.moveaxis(fake, 0, -1)
+    print(fake.shape)
+    
+    m = np.min(fake)
+    M = np.max(fake)
+    
+    fake = (fake-m)/(M-m)
+    
+    fake = 255*fake
+    
+    fake = np.array(fake, dtype='uint8')
+    
+    cv2.imshow('Window', fake)
+    cv2.waitKey(0)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
